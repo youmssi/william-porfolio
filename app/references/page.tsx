af6e6eabd345638f0b8517f12ce8d87333289c03
kengnidoriane <dorianekengni@gmail.com>
2025-07-29 12:26:46 +0100

    delete ..

--- app/references/page.tsx
+++ app/references/page.tsx
@@ -39,10 +39,10 @@ const references: Reference[] = [
     institution: "SIEMENS SA/NV Belgium, Guido Gezellestraat123",
     mobile: "+32 466 186 902 / (+237) 691 499 818",
     whatsapp: "https://wa.me/32466186902",
-    email: "ndachthiery@yahoo.com",
+    email: "ndach_thierry@yahoo.com",
     profile: {
-      label: "thierryndoh.ndach@siemens.com",
-      url: "mailto:thierryndoh.ndach@siemens.com"
+      label: "thierry_ndoh.ndach@siemens.com",
+      url: "mailto:thierry_ndoh.ndach@siemens.com"
     },
     professionalIds: [],
     researchInterest: "Scada Transport and Distribution Control centers. System architect for grid control applications for Transport and Distribution networks. Versed with the use and implementation of Transmission Network application (SE, CA, PF, etc). Control Center SCADA software, Design Engineering, Installation and Commissioning."
