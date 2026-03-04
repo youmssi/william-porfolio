8a61c2ed8ecace52ffd707c9048c55aba674725a
mrvin100 <vincentyoumssi@gmail.com>
2026-03-04 10:11:30 +0100

    fix: update CV path to catch the correct from public folder

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -95,7 +95,7 @@ export function ContactSection() {
               </div>
               <div className="pt-6 flex flex-col gap-3">
                 <Button asChild variant="outline">
-                  <Link href="/CV_TIMOH WILLIAM _CHICK.pdf" download>
+                  <Link href="/CV_TIMOH_WILLIAM_CHICK.pdf" download={true}>
                     Download CV <Download className="ml-2 h-4 w-4" />
                   </Link>
                 </Button>
